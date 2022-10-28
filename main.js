786c7ec300f4f4e2a673d2a0e23a9082d35f7658
Dthwalker <93334122+Dthwalker@users.noreply.github.com>
2022-10-28 16:44:58 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+/* ————— SEO Test by DTHWalker —————*/
+
+class DTHWalkerTests {
+
+  checkTitle() {
+    let x = document.querySelector('title');
+    if (x) {
+      console.log(x);
+    } else {
+      console.log('Title not found');
+    }
+  }
+
+  checkH1() {
+    let x = document.getElementsByTagName('h1');
+    if (x.length == 0) {
+      console.log('H1 not found');
+      return
+    }
+    if (x.length > 1) {
+      console.log('H1 more then 1:');
+      console.log (x);
+      return
+    }
+    console.log('H1 found:');
+    console.log (x[0]);
+  }
+
+  checkKeys(x) {
+    let word
+    typeof x == 'object' ? word = x[this.t] : word = x
+    const regexp = RegExp(word,'gi');
+    let y = document.body.innerText.match(regexp);
+    if (y) {
+      console.log(word + ' : ' + y.length)
+    } else {
+      console.log(`Key '${word}' not found`)
+    }
+    if (typeof x == 'object') {
+      if (this.t < x.length - 1) {
+        this.t++
+        this.checkKey(x)
+      } else {
+        this.t = 0
+      }
+    }
+  }
+
+}
+
+const dthwTest = new DTHWalkerTests()
